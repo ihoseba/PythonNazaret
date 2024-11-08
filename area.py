3f0b1c664542b201fdc0e2c8369129705efea256
ihoseba <125491142+ihoseba@users.noreply.github.com>
2024-11-08 15:43:20 +0100

    Add files via upload

---
+++ area.py
@@ -0,0 +1,14 @@
+import sys
+
+# arg 0, es x
+# arg 1, es y
+# Calcula area,
+# llamada python area.py x y
+# ejemplo python area.py 3 4
+
+x=int(sys.argv[1])
+y=int(sys.argv[2])
+# x=3
+# y=4
+area=x*y
+print("Area es: ",area)
